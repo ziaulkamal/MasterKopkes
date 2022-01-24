5f3349d24193aca4eaee04e35d8b2a3df8f071f0
ziaulkamal <codepad@usa.com>
2022-01-24 20:19:38 +0700

    :start: Source Dasar

---
+++ application/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller{
+
+  public function __construct()
+  {
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+  }
+
+  function index()
+  {
+      $data = array(
+        'page' => '',
+      );
+      $this->load->view('main', $data);
+  }
+
+}
